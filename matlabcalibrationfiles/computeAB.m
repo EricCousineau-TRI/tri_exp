f5fa0d0c460bfa3ae967c43643f153f7e98acd64
Jiaji Zhou <robinzhou55@gmail.com>
2017-07-21 12:58:46 -0400

    refit em algorithm

--- matlabcalibrationfiles/computeAB.m
+++ matlabcalibrationfiles/computeAB.m
@@ -8,10 +8,10 @@ for i = 2:1:num_poses
 %     B =[ B tf_april_to_camera{i} * inv(tf_april_to_camera{i-1}) ];
 %      A =[ A inv(tf_ee_to_base{i}) * tf_ee_to_base{1}];
 %     B =[ B tf_april_to_camera{i} * inv(tf_april_to_camera{1}) ];
-%A =[ A, inv(tf_ee_to_base{i-1}) * (tf_ee_to_base{i})  ];
-%B =[ B, tf_april_to_camera{i-1} * inv(tf_april_to_camera{i}) ];
-A =[ A (tf_ee_to_base{1}) \ (tf_ee_to_base{i})  ];
-B =[ B tf_april_to_camera{1} / (tf_april_to_camera{i}) ];
+A =[ A, inv(tf_ee_to_base{i-1}) * (tf_ee_to_base{i})  ];
+B =[ B, tf_april_to_camera{i-1} * inv(tf_april_to_camera{i}) ];
+%A =[ A (tf_ee_to_base{1}) \ (tf_ee_to_base{i})  ];
+%B =[ B tf_april_to_camera{1} / (tf_april_to_camera{i}) ];
 
 end
 end
