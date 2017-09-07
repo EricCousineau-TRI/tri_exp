d19cc3915062abe2cba287c6d7e6ad0332df71f3
Eric Cousineau <eric.cousineau@tri.global>
2017-09-07 16:59:22 -0400

    Further flesh out

--- src/pose_est_sim.cc
+++ src/pose_est_sim.cc
@@ -3,54 +3,23 @@
 #include "drake/systems/sensors/image.h"
 #include "drake/systems/sensors/lcm_image_array_t_to_image.h"
 
+#include "drake/examples/kuka_iiwa_arm/dev/push_and_pick/perception_base.h"
+
+using namespace drake::systems::sensors;
+using namespace drake::examples::kuka_iiwa_arm::push_and_pick;
+
 namespace tri_exp {
 
 const std::string kPath =
     "drake/manipulation/models/iiwa_description/urdf/"
     "iiwa14_polytope_collision.urdf";
 
-class DrakeCameraInterface {
- public:
-  void UpdateReading(double time,
-                     const ImageRgba8U& color_image,
-                     const ImageDepth32F& depth_image) {
-    state.time = time;
-    state.color_image = color_image;
-    state.depth_image = depth_image;
-  }
-
-  void GetCurrentPointCloud(pcl::PointCloud<ColoredPointT>::Ptr cloud) {
-    // Get image.
-    Matrix3Xf points = RgbdCamera::ToPointCloud(camera_info, state.depth_image);
-    // Convert to PCL point cloud.
-    const int n = points.cols();
-    cloud.resize(n);
-    Pixel* img = color_image.data();
-    int k = 0;
-    for (int i = 0; i < n; ++i) {
-      cloud.point[i].xyz.EigenMap() = points.col(i);
-      const int u = i % width;
-      const int v = i / width;
-      cloud.point[i].color = img[k];
-      ++k;
-    }
-  }
- private:
-  struct State {
-    double time;
-    ImageRgba8U color_image;
-    ImageDepth32F depth_image;
-  };
-  State state_;
-};
-
-
 Isometry3d GetBookPose(pcl::PointCloud<ColoredPointT>::Ptr cloud_in) {
   PointCloudPerception<ColoredPointT, ColoredPointTNormal> perception_proc;
   DrakeCameraInterface camera_interface;
 
   // Need a copy...
-  pcl::PointCLoud<ColoredPointT>::Ptr cloud(cloud_in);
+  pcl::PointCloud<ColoredPointT>::Ptr cloud(cloud_in);
 
   perception_proc.OutlierRemoval(cloud);
 
@@ -81,7 +50,63 @@ Isometry3d GetBookPose(pcl::PointCloud<ColoredPointT>::Ptr cloud_in) {
   std::cout << "Orientation: " << orientation << std::endl;
   std::cout << "Yaw angle: " << yaw_angle_radian * 180 / M_PI << std::endl;
 
-  return 0;
+  Isometry3d X_WO;
+  X_WO.setIdentity();
+  X_WO.translation() = center;
+  X_WO.linear() << orientation;
 }
 
+class PerceptionImpl : public PerceptionBase {
+ public:
+  PerceptionImpl()
+      : PerceptionBase(CameraInfo()) {
+    // Use camera_info from simulation.
+    cloud_fused_.reset(new PointCloud<ColoredPointT>());
+  }
+
+  void Update(double time,
+              const ImageDepth32F& depth_image,
+              const Isometry3d& X_WD) {
+    // Fuse point cloud.
+    RgbdCamera::ConvertDepthImageToPointCloud(depth_image, camera_info,
+      &points_D);
+    Matrix3Xf points_W = X_WD * points_D;
+
+    pcl::PointCloud<ColoredPointT>::Ptr
+        cloud_W(new pcl::PointCloud<ColoredPointT>());
+    int n = points_W.cols();
+    cloud_W->resize(n);
+
+    // int k = 0;
+    for (int i = 0; i < n; ++i) {
+      auto& p = cloud_W.point[i];
+      auto pt_W = points_W.col(i);
+      p.x = pt_W[0];
+      p.y = pt_W[1];
+      p.z = pt_W[2];
+      // const int u = i % width;
+      // const int v = i / width;
+      // cloud_W.point[i].color = img[k];
+      // ++k;
+    }
+
+    *cloud_fused_ += cloud_W;
+  }
+
+  Isometry3d EstimatePose() {
+    return GetBookPose(cloud_fused_);
+  }
+
+ private:
+  PointCloud<ColoredPointT>::Ptr cloud_fused_;
+};
+
 }  // namespace tri_exp
+
+using namespace tri_exp;
+
+int main() {
+  DoMain(new PerceptionImpl());
+
+  return 0;
+}
