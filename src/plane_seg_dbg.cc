bf4e964da7dd597235054af45d536c51a36980b9
Eric Cousineau <eric.cousineau@tri.global>
2017-09-11 16:23:18 -0400

    Can repro the odd segementation stfuf

--- src/plane_seg_dbg.cc
+++ src/plane_seg_dbg.cc
@@ -1,3 +1,5 @@
+#include <cstdlib>
+
 #include <boost/make_shared.hpp>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
@@ -22,6 +24,25 @@
 #include <pcl/search/impl/search.hpp>
 #include <pcl/visualization/impl/pcl_visualizer.hpp>
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/lccp_segmentation.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/integral_image_normal.h>
+
+#include <pcl/octree/octree_search.h>
+
 using namespace std;
 
 typedef pcl::PointXYZRGB ColoredPointT;
@@ -30,7 +51,7 @@ typedef pcl::PointXYZ PointT;
 typedef ColoredPointT T;
 
 int main() {
-  const string file_name = "pre_subtract.pcd";
+  const string file_name = "src/pre_subtract.pcd";
 
   boost::shared_ptr<pcl::PointCloud<T>> cloud(new pcl::PointCloud<T>());
 
@@ -40,14 +61,45 @@ int main() {
   pcl::removeNaNFromPointCloud(*cloud,*cloud, indices);
   std::cout << "remove Nan size" << indices.size() << std::endl;
 
+  // int x = system("bash -c pwd");
+  // (void)x;
+
+  double dist_threshold = 0.01;
+  pcl::PointIndices::Ptr inliers(new pcl::PointIndices ());
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients());
+
+  pcl::SACSegmentation<T> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType(pcl::SACMODEL_PLANE);
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setDistanceThreshold(dist_threshold);
+
+  seg.setInputCloud(cloud);
+  seg.segment(*inliers, *coefficients);
+
+  int num_before = cloud->size();
+  pcl::ExtractIndices<T> extract;
+  extract.setInputCloud (cloud);
+  extract.setIndices (inliers);
+
+  typename pcl::PointCloud<T>::Ptr plane(new pcl::PointCloud<T>());
+  typename pcl::PointCloud<T>::Ptr non_plane(new pcl::PointCloud<T>());
+  extract.filter(*plane);
+
+  extract.setNegative (true);
+  extract.filter(*non_plane);
+
   // Visualize.
   pcl::visualization::PCLVisualizer viewer("Point Cloud Visualization");
   auto tf = Eigen::Affine3f::Identity();
   viewer.addCoordinateSystem(0.2, tf);
-  pcl::visualization::PointCloudColorHandlerCustom<ColoredPointT> single_color (cloud, 0, 255, 0);
-  // viewer.addPointCloud(cloud, rgb, "Cloud");
-  viewer.addPointCloud(cloud, single_color, "Cloud");
-  std::cout << "to display" << std::endl;
+  pcl::visualization::PointCloudColorHandlerCustom<ColoredPointT> green(cloud, 0, 255, 0);
+  pcl::visualization::PointCloudColorHandlerCustom<ColoredPointT> red(cloud, 255, 0, 0);
+  viewer.addPointCloud(plane, green, "Plane");
+  viewer.addPointCloud(non_plane, red, "Non-plane");
+
   // Display the point cloud until 'q' key is pressed.
   while (!viewer.wasStopped ()) {
     viewer.spinOnce ();
